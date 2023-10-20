ae1843d132d4189233bc71ad27e07b75778d19c7
Kdocia <134646850+Kdocia@users.noreply.github.com>
2023-10-19 22:41:50 -0300

    Finalização do Código
    #Definição das regras para nome de usuários
    #Inserção da hashlib e criptografia das senhas usando sha256
    #Inserção da json para o armazenamento dos usuarios criados

--- usuario.py
+++ usuario.py
@@ -1,15 +1,41 @@
+import json
+from hashlib import sha256
+
+#Criação da classe User
 class User:
     def __init__(self, nome, senha, cargo):
         self.nome = nome
         self.senha = senha
         self.cargo = cargo
 
+#Definição dos parametros que irão ser utilizados para o nome do usuario
     def set_nome(self):
-        self.nome = input('Insira seu nome de Usuário:\n')
+        print(' - Minímo 4 caracteres\n - Máximo 16 caracteres\n - Não pode conter espações em branco\n')
+        while True:
+            self.nome = input("User: ")
+            user_sem_espacos = self.nome.replace(" ", "")
+            if user_sem_espacos == self.nome:
+                while True:
+                    if 3 >= len(self.nome) > 0:
+                        print("Nome deve ter no mínimo 4 caracter")
+                        self.nome = input("User: ")
 
+                    elif 16 >= len(self.nome) >= 4:
+                        print("User valido")
+                        break
+                    else:
+                        print("O nome deve ter no maximo 16 caracter")
+                        self.nome = input("User: ")
+                break
+            else:
+                print("Não é permintido espaços em branco")
+
+#Definição dos parametros que irão ser utilizados para a criação da senha, sendo ela armazenada de forma criptografada com sha256 em hexadecimal
     def set_senha(self):
-        self.senha = input('Insira sua Senha:\n')
-        check = input('Confimre sua senha:\n')
+        senha_inserida = input('Insira sua Senha:\n')
+        hash_senha = sha256(senha_inserida.encode())
+        self.senha = hash_senha.hexdigest()
+        check = sha256(input('Confimre sua senha:\n').encode()).hexdigest()
         if self.senha == check:
             pass
         else:
@@ -18,6 +44,7 @@ class User:
                 self.senha = input("Insira sua Senha novamente:\n")
                 check = input('Confimre sua senha:\n')
 
+#Definição dos parametros que irão ser utilizados para definir o cargo de cada usuario
     def set_cargo(self):
         print('Cargos Dispiníveis:\n [1] Visualizador   \n [2] Padrão \n [3] Administrador  \n [4] Super Administrador \n Qual é o tipo de cargo desse usuário?')
         while True:
@@ -30,7 +57,7 @@ class User:
                                 self.cargo = 'Visualizador'
                     elif n_cargo == 2:
                                 print('Usuário configurado como Padrão')
-                                self.cargo = 'Padrão'
+                                self.cargo = 'Padrao'
                     elif n_cargo == 3:
                                 print('Usuário configurado como Administrador')
                                 self.cargo = 'Administrador'
@@ -45,16 +72,39 @@ class User:
             except ValueError:
                 print('Cargo não identificado')
 
+#Sucesso ao criar um usuario
     def ssuccess(self):
         print(f'Usuário criado com sucesso! \nBem vindo {self.nome}, você é o mais novo Usuário {self.cargo}')
 
+lista_de_usuarios=[]
 
+#enquanto o loop rodar será permitido criar varios usuarios e definir seus cargos e tudo será salvo em usuarios.json
+while True:  
+    print('!Crie um usuário! ou !Digite "sair" para encerrar!')
+    usuario = User('standard', 'standard', 'standard')
+    usuario.set_nome()
 
-usuario = User('standard', 'standard', 'standard') 
+    nome = usuario.nome
+    if nome.lower() == 'sair':  #Comando sair permite que se encerre a criação de usuarios
+        break
+    
+    usuario.set_senha()
+    senha = usuario.senha
 
-print('!Crie seu usuário!')
-usuario.set_nome()
-usuario.set_senha()
-usuario.set_cargo()
-usuario.ssuccess()
+    usuario.set_cargo()
+    cargo = usuario.cargo
 
+#Criação do dicionario com informações especificas de cada usuarios 
+    dicionario_user = {
+        "User": nome,
+        "Senha": senha,
+        "Cargo": cargo
+    }
+
+#Inserir as informações do usuario criado em uma lista cque comportarão todos os usuarios criado
+    lista_de_usuarios.append(dicionario_user)
+    usuario.ssuccess() 
+
+#Escreve a lista criada no aquivo usuarios.json
+with open ("usuarios.json","w") as arquivo_json:
+    json.dump(lista_de_usuarios,arquivo_json)
